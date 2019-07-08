e6cfee04efca5b6ae60038ea70abbc6af008ce8f
Stefan Adams <s@adams.fm>
2019-07-08 09:22:18 -0500

    add remove alias capability to CLI

---
+++ lib/Mailroom/Command/remove.pm
@@ -0,0 +1,65 @@
+package Mailroom::Command::remove;
+use Mojo::Base 'Mojolicious::Command';
+
+has description => 'Remove Mailroom aliases';
+has usage => sub { shift->extract_usage };
+
+sub run {
+  my ($self, $recipient) = @_;
+
+  $self->app->pg->db->delete('aliases', {recipient => $recipient});
+}
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Mailroom::Command::remove - Remove mailroom aliases
+
+=head1 SYNOPSIS
+
+  Usage: APPLICATION remove [ALIASES]
+
+    ./myapp.pl remove recipient
+
+=head1 DESCRIPTION
+
+L<Mailroom::Command::remove> removes aliases to L<Mailroom>.
+
+=head1 ATTRIBUTES
+
+L<Mailroom::Command::remove> inherits all attributes from
+L<Mojolicious::Command> and implements the following new ones.
+
+=head2 description
+
+  my $description = $job->description;
+  $job            = $job->description('Foo');
+
+Short description of this command, used for the command list.
+
+=head2 usage
+
+  my $usage = $job->usage;
+  $job      = $job->usage('Foo');
+
+Usage information for this command, used for the help screen.
+
+=head1 METHODS
+
+L<Mailroom::Command::remove> inherits all methods from
+L<Mojolicious::Command> and implements the following new ones.
+
+=head2 run
+
+  $job->run(@ARGV);
+
+Run this command.
+
+=head1 SEE ALSO
+
+L<Mailroom>, L<Mojolicious::Guides>, L<http://mojolicious.org>.
+
+=cut
