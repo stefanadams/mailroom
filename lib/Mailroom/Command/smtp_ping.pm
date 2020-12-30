ab36eb80486220bc2847b4e7a2b7bb4dea554772
Stefan Adams <stefan@adams.fm>
2020-12-30 22:33:46 +0000

    add status checking

---
+++ lib/Mailroom/Command/smtp_ping.pm
@@ -0,0 +1,64 @@
+package Mailroom::Command::smtp_ping;
+use Mojo::Base 'Mojolicious::Command';
+
+has description => 'Send a ping email';
+has usage => sub { shift->extract_usage };
+
+sub run {
+  my $self = shift;
+  $self->app->smtp_ping(@_);
+}
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Mailroom::Command::lookup - Lookup mailroom aliases
+
+=head1 SYNOPSIS
+
+  Usage: APPLICATION lookup recipient
+
+    ./myapp.pl lookup recipient [recipient ...]
+
+=head1 DESCRIPTION
+
+L<Mailroom::Command::lookup> looks up aliases in L<Mailroom>.
+
+=head1 ATTRIBUTES
+
+L<Mailroom::Command::lookup> inherits all attributes from
+L<Mojolicious::Command> and implements the following new ones.
+
+=head2 description
+
+  my $description = $job->description;
+  $job            = $job->description('Foo');
+
+Short description of this command, used for the command list.
+
+=head2 usage
+
+  my $usage = $job->usage;
+  $job      = $job->usage('Foo');
+
+Usage information for this command, used for the help screen.
+
+=head1 METHODS
+
+L<Mailroom::Command::lookup> inherits all methods from
+L<Mojolicious::Command> and implements the following new ones.
+
+=head2 run
+
+  $job->run(@ARGV);
+
+Run this command.
+
+=head1 SEE ALSO
+
+L<Mailroom>, L<Mojolicious::Guides>, L<http://mojolicious.org>.
+
+=cut
