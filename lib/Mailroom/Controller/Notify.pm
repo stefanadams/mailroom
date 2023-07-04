9fa88969a97057e4b343e76dc0ed331e8ac30cd4
Stefan Adams <stefan@adams.fm>
2023-07-04 14:33:27 -0500

    disable notify jobs

--- lib/Mailroom/Controller/Notify.pm
+++ lib/Mailroom/Controller/Notify.pm
@@ -5,6 +5,7 @@ sub notify ($self) {
   my $domain = $self->mx or return $self->reply->not_found;
   my $json = $self->req->json;
   $self->log->debug(sprintf 'logging %s /notify %s maintenance task events (%s)', scalar(@$json), $domain, join ',', map { $_->{event} } @$json);
+  return $self->reply->ok;
   my $id = $self->minion->enqueue(notify => [$domain, map { $_->{event} } @$json] => {queue => 'maintenance'});
   $self->minion->job($id)->note(events => $json);
   $self->render(json => {notify => [map { $_->{event} } @$json], domain => $domain});
